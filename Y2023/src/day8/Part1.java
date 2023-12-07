6a288c929ece14e7d4c3db7f0f0e27aefd9a7275
nyanSpruk <nik.janspruk@gmail.com>
2023-12-07 21:20:31 +0100

    README.md and day8 preparations

---
+++ Y2023/src/day8/Part1.java
@@ -0,0 +1,40 @@
+package day8;
+
+import java.util.List;
+
+public class Part1 {
+
+    private static int publicInputSolution = 0;
+
+    private static int hiddenInputSolution = 0;
+
+    public static void run(List<String> input, boolean isHidden)
+    {
+        if (isHidden)
+            setHiddenInputSolution(solve(input));
+        else
+            setPublicInputSolution(solve(input));
+    }
+
+    private static int solve(List<String> input)
+    {
+        return input.size();
+    }
+
+    public static int getPublicInputSolution() {
+        return publicInputSolution;
+    }
+
+    public static void setPublicInputSolution(int val) {
+        publicInputSolution = val;
+    }
+
+    public static int getHiddenInputSolution() {
+        return hiddenInputSolution;
+    }
+
+    public static void setHiddenInputSolution(int val) {
+        hiddenInputSolution = val;
+    }
+
+}
