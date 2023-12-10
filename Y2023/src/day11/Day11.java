313a45c676672c5372e885a0081252f5416d5e49
nyanSpruk <nik.janspruk@gmail.com>
2023-12-10 17:18:33 +0100

    day11 prep

---
+++ Y2023/src/day11/Day11.java
@@ -0,0 +1,37 @@
+package day11;
+
+import utils.FileReader;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Day11 {
+    public static void main(String[] args) {
+        try {
+            int day = 11;
+
+            List<String> publicInput1 = FileReader.readPublic(day, 1);
+            List<String> publicInput2 = FileReader.readPublic(day, 2);
+
+            List<String> hiddenInput = FileReader.readHidden(day);
+
+            // Part 1
+            Part1.run(publicInput1, false);
+            System.out.println("Part 1 day " + day + " public result: " + Part1.getPublicInputSolution());
+
+            Part1.run(hiddenInput, true);
+            System.out.println("Part 1 day " + day + " hidden result: " + Part1.getHiddenInputSolution());
+
+            // Part 2
+            Part2.run(publicInput2, false);
+            System.out.println("Part 2 day " + day + " public result: " + Part2.getPublicInputSolution());
+
+            Part2.run(hiddenInput, true);
+            System.out.println("Part 2 day " + day + " hidden result: " + Part2.getHiddenInputSolution());
+
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+}
