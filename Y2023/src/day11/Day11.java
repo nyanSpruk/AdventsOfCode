b1685c95690b4b2d59612020a1919c666225c767
nyanSpruk <nik.janspruk@gmail.com>
2023-12-11 08:50:48 +0100

    Day11

--- Y2023/src/day11/Day11.java
+++ Y2023/src/day11/Day11.java
@@ -1,9 +1,9 @@
 package day11;
 
-import utils.FileReader;
-
 import java.io.IOException;
 import java.util.List;
+
+import utils.FileReader;
 
 public class Day11 {
     public static void main(String[] args) {
@@ -16,18 +16,18 @@ public class Day11 {
             List<String> hiddenInput = FileReader.readHidden(day);
 
             // Part 1
-            Part1.run(publicInput1, false);
+            Part1.run(publicInput1, false, 2);
             System.out.println("Part 1 day " + day + " public result: " + Part1.getPublicInputSolution());
 
-            Part1.run(hiddenInput, true);
+            Part1.run(hiddenInput, true, 2);
             System.out.println("Part 1 day " + day + " hidden result: " + Part1.getHiddenInputSolution());
 
             // Part 2
-            Part2.run(publicInput2, false);
-            System.out.println("Part 2 day " + day + " public result: " + Part2.getPublicInputSolution());
+            Part1.run(publicInput2, false, 1000000);
+            System.out.println("Part 2 day " + day + " public result: " + Part1.getPublicInputSolution());
 
-            Part2.run(hiddenInput, true);
-            System.out.println("Part 2 day " + day + " hidden result: " + Part2.getHiddenInputSolution());
+            Part1.run(hiddenInput, true, 1000000);
+            System.out.println("Part 2 day " + day + " hidden result: " + Part1.getHiddenInputSolution());
 
         } catch (IOException e) {
             //noinspection CallToPrintStackTrace
--- Y2023/src/day11/Part1.java
+++ Y2023/src/day11/Part1.java
@@ -1,38 +1,121 @@
 package day11;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Part1 {
 
-    private static int publicInputSolution = 0;
+    private static long publicInputSolution = 0;
 
-    private static int hiddenInputSolution = 0;
+    private static long hiddenInputSolution = 0;
 
-    public static void run(List<String> input, boolean isHidden) {
+    public static void run(List<String> input, boolean isHidden, int size)
+    {
         if (isHidden)
-            setHiddenInputSolution(solve(input));
+            setHiddenInputSolution(solve(input, size));
         else
-            setPublicInputSolution(solve(input));
+            setPublicInputSolution(solve(input, size));
     }
 
-    private static int solve(List<String> input) {
-        return input.size();
+    private static long solve(List<String> input, int size)
+    {
+        List<Integer> rows = new ArrayList<>();
+
+        for (int i = 0; i < input.size(); i++)
+            if (isAllDots(input.get(i)))
+                rows.add(i);
+
+        List<Integer> indexes = new ArrayList<>();
+        for (int j = 0; j < input.get(0).length(); j++)
+        {
+            int finalJ = j;
+            String column = input.stream()
+                    .map(row -> String.valueOf(row.charAt(finalJ)))
+                    .collect(Collectors.joining());
+
+            if (isAllDots(column))
+                indexes.add(j);
+        }
+
+        List<Point> points = new ArrayList<>();
+        for (int r = 0; r < input.size(); r++)
+        {
+            String row = input.get(r);
+            for (int c = 0; c < row.length(); c++)
+            {
+                if (row.charAt(c) == '#')
+                {
+                    int count = 0;
+                    for (int rowI : rows)
+                        if (rowI < r)
+                            count += size - 1;
+
+                    int count2 = 0;
+                    for (int colI : indexes)
+                        if (colI < c)
+                            count2 += size - 1;
+
+                    points.add(new Point(c + count2, r + count));
+                }
+            }
+        }
+        long sum = 0;
+
+        for (int i = 0; i < points.size(); i++)
+        {
+            for (int j = i + 1; j < points.size(); j++)
+            {
+                Point point1 = points.get(i);
+                Point point2 = points.get(j);
+                int distance = calculateManhattanDistance(point1, point2);
+                sum += distance;
+                //                System.out.println("Shortest distance between #" + (i + 1) + " and #" + (j + 1) + ": " + (distance));
+            }
+        }
+        return sum;
     }
 
-    public static int getPublicInputSolution() {
+    private static int calculateManhattanDistance(Point point1, Point point2)
+    {
+        return Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y);
+    }
+
+    private static boolean isAllDots(String str)
+    {
+        return str.matches("\\.+");
+    }
+
+    public static long getPublicInputSolution()
+    {
         return publicInputSolution;
     }
 
-    public static void setPublicInputSolution(int val) {
+    public static void setPublicInputSolution(long val)
+    {
         publicInputSolution = val;
     }
 
-    public static int getHiddenInputSolution() {
+    public static long getHiddenInputSolution()
+    {
         return hiddenInputSolution;
     }
 
-    public static void setHiddenInputSolution(int val) {
+    public static void setHiddenInputSolution(long val)
+    {
         hiddenInputSolution = val;
+    }
+
+    static class Point
+    {
+        int x;
+        int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 
 }
